Update to fix some basic compile errors. Ready to work on acceleration calculation

Planets.py
@@ -1,5 +1,5 @@
-import math
-from scipy import scipy.gravitational_constant
+#import math
+from scipy import constants
 
 class Position():
     def __init__(self, x, y, z):
@@ -25,7 +25,7 @@ class Force():
 
 class Object():
     def __init__(self, id):
-        self.id = 0
+        self.id = id
         self.charge = 0
         self.mass = 0
         self.position = Position(0, 0, 0)
@@ -34,19 +34,19 @@ class Object():
         self.print_self()
 
     def on_step(self, time, universe):
-        print("Calculating vectors for item {}".format(self.id))
+        print("Calculating vectors for item {0}".format(self.id))
         self.calculate_position(time, universe)
 
     def calculate_acceleration(self, time, universe):
         print("Calculating acceleration.")
         for item in universe:
-            if item.id != self.id:
-                return (self.mass * item.mass)/((math.abs(self.x - item.x)))
+            if item.id != self.id and False:
+                return (self.mass * item.mass)/((abs(self.position.x - item.position.x)))
             else:
                 return 0
 
     def get_impact_gravity(self, item):
-        return (self.mass * item.mass)/((math.abs(self.x - item.x)))
+        return (self.mass * item.mass)/((abs(self.position.x - item.position.x)))
 
     def calculate_velocity(self, time, universe):
         self.calculate_acceleration(time, universe)
@@ -58,10 +58,10 @@ class Object():
         
 
     def print_self(self):
-        print("Object Defined:\n\tMass:{0}\n\tCharge:{1}\n\tPosition: ({2},{3})"
-            .format(self.charge, self.mass, self.position.x, self.position.y))
+        print("Object {0}:\n\tMass:{1}\n\tCharge:{2}\n\tPosition: ({3},{4})"
+            .format(self.id, self.charge, self.mass, self.position.x, self.position.y))
 
-print (gravitational_constant)
+print( constants.gravitational_constant)
 destination = Object("Destination")
 source = Object("Source")
 focus = Object("Focus")
@@ -73,14 +73,15 @@ universe = [
 ]
 running = True
 time = 0
+
 print("Program Started... Press any key to terminate.")
 try:
     while True:
         time = time + 1
         for item in universe:
             item.on_step(time, universe)
+            item.print_self()
         print(time)
 except KeyboardInterrupt:
     pass
-
 print("Program has ended")